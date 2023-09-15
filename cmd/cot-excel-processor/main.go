8471e5091933fd2e16ed7e96cd75d0b0d8c77ef2
paulschick <paulschick19@gmail.com>
2023-09-15 18:24:28 -0400

    Process file to desired columns with Grate package

--- cmd/cot-excel-processor/main.go
+++ cmd/cot-excel-processor/main.go
@@ -4,8 +4,8 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"github.com/extrame/xls"
 	downloaderService "github.com/paulschick/cot-downloader/pkg/service"
+	grate "github.com/pbnjay/grate/xls"
 	"os"
 	"path/filepath"
 )
@@ -24,34 +24,10 @@ func main() {
 		panic(err)
 	}
 
-	filePath, err := getFilePath(files[0], dataDir)
+	filePath, err := GetFilePath(files[0], dataDir)
 	if err != nil {
 		panic(err)
 	}
-
-	orderedColNames := []string{
-		"Market_and_Exchange_Names",
-		"Report_Date_as_MM_DD_YYYY",
-		"Open_Interest_All",
-		"NonComm_Positions_Long_All",
-		"NonComm_Positions_Short_All",
-		"Comm_Positions_Long_All",
-		"Comm_Positions_Short_All",
-		"NonRept_Positions_Long_All",
-		"NonRept_Positions_Short_All",
-	}
-
-	workbook, err := OpenExcel(filePath)
-	if err != nil {
-		panic(err)
-	}
-	sheet, err := GetFirstSheet(workbook)
-	if err != nil {
-		panic(err)
-	}
-	columnNamesIndex := getColumnIndices(sheet, orderedColNames)
-
-	fmt.Println(columnNamesIndex)
 
 	outputFile, err := os.Create("output.csv")
 	if err != nil {
@@ -67,37 +43,61 @@ func main() {
 
 	writer := csv.NewWriter(outputFile)
 
-	file, err := xls.Open(filePath, "utf-8")
-	if err != nil {
-		panic(err)
+	fmt.Println("Reading ", filePath)
+
+	orderedColNames := []string{
+		"Market_and_Exchange_Names",
+		"Report_Date_as_MM_DD_YYYY",
+		"Open_Interest_All",
+		"NonComm_Positions_Long_All",
+		"NonComm_Positions_Short_All",
+		"Comm_Positions_Long_All",
+		"Comm_Positions_Short_All",
+		"NonRept_Positions_Long_All",
+		"NonRept_Positions_Short_All",
+	}
+	columnNamesIndex := make(map[string]int)
+
+	for _, colName := range orderedColNames {
+		columnNamesIndex[colName] = -1
 	}
 
-	if sheet := file.GetSheet(0); sheet != nil {
-		for i := 0; i <= int(sheet.MaxRow); i++ {
-			row := sheet.Row(i)
-			var csvRow []string
+	wb, _ := grate.Open(filePath)
+	sheets, _ := wb.List()
+	for _, s := range sheets {
+		sheet, _ := wb.Get(s)
+		i := 0
+		for sheet.Next() {
+			row := sheet.Strings()
+			if i == 0 {
+				for colIdx := 0; colIdx < len(row); colIdx++ {
+					col := row[colIdx]
+					if currentColIndex, ok := columnNamesIndex[col]; ok && currentColIndex == -1 {
+						columnNamesIndex[col] = colIdx
+					}
+				}
+			}
+			csvRow := make([]string, 0)
+
 			for _, colName := range orderedColNames {
 				colIdx := columnNamesIndex[colName]
-				//fmt.Println(colName, colIdx)
 				if colIdx != -1 {
-					csvRow = append(csvRow, row.Col(colIdx))
+					csvRow = append(csvRow, row[colIdx])
 				} else {
 					csvRow = append(csvRow, "")
 				}
-				fmt.Println(csvRow)
 			}
 			err = writer.Write(csvRow)
 			if err != nil {
 				panic(err)
 			}
+
+			i++
 		}
 	}
-
-	writer.Flush()
-	fmt.Println("Done")
 }
 
-func getFilePath(file os.DirEntry, basePath string) (string, error) {
+func GetFilePath(file os.DirEntry, basePath string) (string, error) {
 	if file == nil {
 		return "", fmt.Errorf("file is nil")
 	}
@@ -105,45 +105,6 @@ func getFilePath(file os.DirEntry, basePath string) (string, error) {
 		return "", fmt.Errorf("file is a directory")
 	}
 	return filepath.Join(basePath, file.Name()), nil
-}
-
-func OpenExcel(filePath string) (*xls.WorkBook, error) {
-	file, err := xls.Open(filePath, "utf-8")
-	if err != nil {
-		return nil, err
-	}
-	return file, nil
-}
-
-func GetFirstSheet(workbook *xls.WorkBook) (*xls.WorkSheet, error) {
-	if workbook == nil {
-		return nil, fmt.Errorf("workbook is nil")
-	}
-	if sheet := workbook.GetSheet(0); sheet != nil {
-		return sheet, nil
-	}
-	return nil, fmt.Errorf("sheet is nil")
-}
-
-func getColumnIndices(sheet *xls.WorkSheet, orderedColNames []string) map[string]int {
-	columnNamesIndex := make(map[string]int)
-	for _, colName := range orderedColNames {
-		columnNamesIndex[colName] = -1
-	}
-
-	row1 := sheet.Row(0)
-	foundColumns := 0
-	for idx := row1.FirstCol(); idx < row1.LastCol(); idx++ {
-		colName := row1.Col(idx)
-		if colIndex, ok := columnNamesIndex[colName]; ok && colIndex == -1 {
-			columnNamesIndex[colName] = idx
-			foundColumns++
-			if foundColumns == len(columnNamesIndex) {
-				break
-			}
-		}
-	}
-	return columnNamesIndex
 }
 
 func initReports() {
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/paulschick/cot-excel-processor
 go 1.21.1
 
 require (
-	github.com/extrame/ole2 v0.0.0-20160812065207-d69429661ad7 // indirect
-	github.com/extrame/xls v0.0.1 // indirect
-	github.com/paulschick/cot-downloader v0.0.0-20230913015328-13d734971b27 // indirect
+	github.com/paulschick/cot-downloader v0.0.0-20230913015328-13d734971b27
+	github.com/pbnjay/grate v0.0.0-20220809231734-6f2eabdf189c
 )
